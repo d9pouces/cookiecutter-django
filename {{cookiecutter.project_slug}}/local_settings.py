c630106047d0a87abe58fac55c9adee5f5b9b90f
d9pouces <github@19pouces.net>
2023-04-17 22:12:18 +0200

    ci: update github action

--- {{cookiecutter.project_slug}}/local_settings.py
+++ {{cookiecutter.project_slug}}/local_settings.py
@@ -1,14 +1,16 @@
+# ##############################################################################
+#  Copyright (c) 2023.                                                         #
+#  This file local_settings.py is part of GlobalSiteChecker.                   #
+#  Please check the LICENSE file for sharing or distribution permissions.      #
+# ##############################################################################
 """Define settings for local development."""
-import tempfile
+import os
 
 DEBUG = True
-
-MEDIA_ROOT = ".docker/media"
-FILE_UPLOAD_TEMP_DIR = tempfile.gettempdir()
-LOG_DIRECTORY = ".docker/log"
-DATABASE_URL = "postgres://username:password@localhost:5432/database"
-COMMON_REDIS_URL = "redis://:password@localhost:6379/1"
-SERVER_BASE_URL = "http://localhost:8000/"
 LOG_LEVEL = "DEBUG"
-ADMIN_EMAIL = "asterix@19pouces.net"
-LISTEN_ADDRESS = "127.0.0.1:8000"
+if "CI" not in os.environ:
+    LOCAL_PATH = ".docker"
+    LANGUAGE_CODE = "fr-FR"
+    LISTEN_ADDRESS = "127.0.0.1:8000"
+    DATABASE_URL = "postgres://username:password@127.0.0.1:5432/database"
+    COMMON_REDIS_URL = "redis://:password@127.0.0.1:6379/1"
